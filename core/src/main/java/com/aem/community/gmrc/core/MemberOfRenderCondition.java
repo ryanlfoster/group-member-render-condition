a32d2e78b56848a15df2cdd5ab577f2247d4abec
Web Comm <webcomm@webs-imac.edw2.uc.edu>
2016-05-19 14:49:34 -0400

    first commit

---
+++ core/src/main/java/com/aem/community/gmrc/core/MemberOfRenderCondition.java
@@ -0,0 +1,59 @@
+package com.aem.community.gmrc.core;
+
+import java.util.Iterator;
+
+import javax.jcr.RepositoryException;
+import javax.servlet.ServletException;
+
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.Group;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.adobe.granite.ui.components.rendercondition.RenderCondition;
+
+public class MemberOfRenderCondition implements RenderCondition {
+	
+	String authorizableID;
+	String[] groups;
+	boolean declaredOnly;
+	UserManager userManager;
+
+    private static final Logger log = LoggerFactory.getLogger(MemberOfRenderCondition.class);
+
+	public MemberOfRenderCondition(UserManager userManager, String authorizableID, String[] groups, boolean declaredOnly) {
+		this.authorizableID = authorizableID;
+		this.groups = groups;
+		this.declaredOnly = declaredOnly;
+		this.userManager = userManager;
+		
+	}
+	
+	public boolean check() throws ServletException {
+		
+		try {
+			Authorizable auth = userManager.getAuthorizable(authorizableID);
+			Iterator<Group> ig = auth.memberOf();
+			if(declaredOnly)
+				ig = auth.declaredMemberOf();
+
+			while(ig.hasNext()) {
+				Group g = ig.next();
+				for(String s : groups) {
+					log.error(s);
+					log.error(g.getID());
+					if(g.getID().equals(s.trim())) {
+						return true;
+					}
+				}
+			}
+		} catch (RepositoryException e) {
+			log.error("error",e);
+			return false;
+		}
+
+		return false;
+	}
+	
+}
